d2d4d73e23d1f2334ea79f576bc96c5ac12e167e
UltralyticsAssistant <web@ultralytics.com>
2025-12-18 11:57:44 +0000

    Auto-format by https://ultralytics.com/actions

--- model_info.py
+++ model_info.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # 加载训练好的YOLO模型
-    model = YOLO(r'ultralytics\cfg\models\v3\yolov3-spp.yaml') # 替换为您的模型路径
+    model = YOLO(r"ultralytics\cfg\models\v3\yolov3-spp.yaml")  # 替换为您的模型路径
     # 打印模型的基本信息
     print(model.info())
     # 打印详细的每层结构信息
